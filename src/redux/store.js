9f3e79010cbab2042e2040ae714d4c9a21c8f15d
Awang <awangmedidat98@gmail.com>
2023-07-22 17:43:58 +0700

    Fix-Conflict

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import loginReducer from "./login/slice";
-import listReducer from "./list/slice";
-
-const { combineReducers, configureStore } = require("@reduxjs/toolkit");
-
-const rootReducer = combineReducers({
-  login: loginReducer,
-  list: listReducer,
-});
-
-export default configureStore({
-  reducer: rootReducer,
-});
-=======
 import {applyMiddleware, createStore} from "redux";
 import rootReducer from "./reducers";
 import thunk from "redux-thunk";
@@ -20,4 +5,3 @@ import thunk from "redux-thunk";
 export const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
->>>>>>> d115625 (third commit)
